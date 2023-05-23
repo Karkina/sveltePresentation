b55224301b20156250fe70a18f46481be5592650
XSCORP\Nicolas.Soumare <nicolas.soumare@sita.aero>
2023-05-23 17:04:08 +0200

    codes

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+    import TaskDescription from "./TaskDescription.svelte";
+
+  let tasks = [];
+  let newTask = {id:'',name: '',description :'',type : 'basic'};
+
+  function addTask() {
+	newTask.id = tasks.length;
+    tasks = [...tasks, newTask];
+    newTask = {};
+  }
+
+  function deleteTask(index) {
+    tasks = tasks.filter((_, i) => i !== index);
+  }
+</script>
+
+<h1>Task Manager</h1>
+
+<input type="text" bind:value={newTask.name} placeholder="Enter a task" />
+<input type="text" bind:value={newTask.description} placeholder="Enter a Description" />
+
+<div>
+	<span>Select a type:</span>
+	<button on:click={() => newTask.type = 'basic'} class:selected={newTask.type === 'basic'}>Basic</button>
+	<button on:click={() => newTask.type = 'emergency'} class:selected={newTask.type === 'emergency'}>Emergency</button>
+	<button on:click={() => newTask.type = 'todo'} class:selected={newTask.type === 'todo'}>To Do</button>
+  </div>
+<button on:click={addTask}>Add Task</button>
+
+<TaskDescription tasks={tasks}/>
+   
---
+++ src/TaskDescription.svelte
@@ -0,0 +1,75 @@
+<script>
+    export let tasks;
+    export let description;
+    export let type;
+</script>
+
+<table>
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Basic</th>
+            <th>Emergency</th>
+            <th>ToDo</th>
+        </tr>
+    </thead>
+    <tbody>
+        {#each tasks as task (task.id)}
+            <tr>
+                <td>{task.id}</td>
+                {#if task.type === "basic"}
+                    <td>{task.name}</td>
+                    <td>-</td>
+                    <td>-</td>
+                {:else if task.type === "emergency"}
+                    <td>-</td>
+                    <td>{task.name}</td>
+                    <td>-</td>
+                {:else}
+                    <td>-</td>
+                    <td>-</td>
+                    <td>{task.name}</td>
+                {/if}
+            </tr>
+        {/each}
+    </tbody>
+</table>
+
+<style>
+    table {
+        width: 100%;
+        border-collapse: collapse;
+        margin-top: 20px;
+    }
+
+    th,
+    td {
+        padding: 10px;
+        text-align: left;
+        border-bottom: 1px solid #ccc;
+    }
+
+    th {
+        background-color: #f2f2f2;
+    }
+
+    td {
+        background-color: #fff;
+    }
+
+    tr:nth-child(even) td {
+        background-color: #f9f9f9;
+    }
+
+    .type-basic {
+        background-color: #c7ecee;
+    }
+
+    .type-emergency {
+        background-color: #ff7675;
+    }
+
+    .type-todo {
+        background-color: #74b9ff;
+    }
+</style>
